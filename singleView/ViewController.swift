45cf2ef737d7bd8d86e134aa60f36ea1fbb7cf29
West Kraemer <WestKraemer@gmail.com>
2017-01-10 12:35:22 -0500

    add code to check for prime numbers

--- singleView/ViewController.swift
+++ singleView/ViewController.swift
@@ -10,16 +10,74 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    
+    @IBOutlet var numberTextField: UITextField!
+    
+    @IBOutlet var resultLabel: UILabel!
+    
+    @IBAction func isItPrime(_ sender: Any) {
+        
+        if let userEnteredString = numberTextField.text {
+            
+            let userEnteredInteger = Int(userEnteredString)
+            
+            if let number = userEnteredInteger {
+                
+                var isPrime = true
+                
+                if number == 1 {
+                    
+                    isPrime = false
+                    
+                }
+                
+                var i = 2
+                
+                while i < number {
+                    
+                    if number % i == 0 {
+                        
+                        isPrime = false
+                        
+                    }
+                    
+                    i += 1
+                }
+             
+                if isPrime {
+                    
+                    resultLabel.text = "\(number) is prime!"
+                
+                } else {
+                    
+                    resultLabel.text = "\(number) is not prime."
+                }
+                
+                
+            } else {
+                
+                resultLabel.text = "Please enter a positive whole number"
+            
+            }
+        
+        
+        
+    }
+
+    
+    
+    
+    func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
+    func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
 
+}
 }
 
